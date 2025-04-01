804e85718e677ceced34e6c113de60ddd60043db
FNovitch <franknovitch@hotmail.com>
2025-04-01 16:37:09 -0300

    :sparkles: feat: Axios e  integração com a API de Livros

--- src/pages/QueroDoar/QueroDoar.jsx
+++ src/pages/QueroDoar/QueroDoar.jsx
@@ -1,24 +1,86 @@
 import style from "../QueroDoar/querodoar.module.scss";
 import formImg from "../../assets/formimg.png";
+import axios from "axios";
+import { useState } from "react";
 
-export default function Contact() {
+export default function QueroDoar() {
+  const [título, setTítulo] = useState("");
+  const [categoria, setCategoria] = useState("");
+  const [autor, setAutor] = useState("");
+  const [image_url, setImage_url] = useState("");
+
+  const pegarTítulo = (e) => {
+    setTítulo(e.target.value);
+  };
+  const pegarCategoria = (e) => {
+    setCategoria(e.target.value);
+  };
+  const pegarAutor = (e) => {
+    setAutor(e.target.value);
+  };
+  const pegarImage_url = (e) => {
+    setImage_url(e.target.value);
+  };
+
+  const envioDados = async () => {
+    const dadosParaEnviar = {
+      título,
+      categoria,
+      autor,
+      image_url,
+    };
+    await axios.post(
+      "https://api-livros-t3-9u2p.onrender.com/doar",
+      dadosParaEnviar
+    );
+  };
+
   return (
     <div>
       <span className={style.formtitle}>
-        Por favor, preencha o formulário com suas informações e as informações do livro.
+        Por favor, preencha o formulário com suas informações e as informações
+        do livro.
       </span>
       <section className={style.formcontainer}>
-        <form className={style.form}>
+        <form className={style.form} onSubmit={(e) => [e.preventDefault()]}>
           <div className={style.formmaintext}>
             {" "}
             <img src={formImg} alt="Imagem de um Livro" />
             <p>Informações do Livro</p>
           </div>
-          <input className={style.forminput} type="text" placeholder="Título" required />
-          <input className={style.forminput} type="text" placeholder="Categoria" required />
-          <input className={style.forminput} type="text" placeholder="Autor" required />
-          <input className={style.forminput} type="text" placeholder="Link da Imagem" required />
-          <button className={style.formbutton} type="submit">
+          <input
+            className={style.forminput}
+            type="text"
+            placeholder="Título"
+            onChange={pegarTítulo}
+            required
+          />
+          <input
+            className={style.forminput}
+            type="text"
+            placeholder="Categoria"
+            onChange={pegarCategoria}
+            required
+          />
+          <input
+            className={style.forminput}
+            type="text"
+            placeholder="Autor"
+            onChange={pegarAutor}
+            required
+          />
+          <input
+            className={style.forminput}
+            type="text"
+            placeholder="Link da Imagem"
+            onChange={pegarImage_url}
+            required
+          />
+          <button
+            className={style.formbutton}
+            type="submit"
+            onClick={envioDados}
+          >
             Doar
           </button>
         </form>
